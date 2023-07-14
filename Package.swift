4125233237f59612c3f65b5bfa0a01d3628f813a
Diego Quimbo <dquimbo22@gmail.com>
2023-07-14 10:18:52 -0300

    new version

--- Package.swift
+++ Package.swift
@@ -28,16 +28,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "PBNetworkBinary",
-            path: "./Sources/PBNetworkBinary.xcframework"),
         .target(
             name: "PBNetwokWrapper",
             dependencies: [
                 .product(name: "RxSwift", package: "rxswift"),
                 .product(name: "RealmSwift", package: "realm-swift"),
                 .product(name: "PBBluetooth", package: "calendarcontrol"),
-                .target(name: "PBNetworkBinary")
             ]),
     ]
 )
